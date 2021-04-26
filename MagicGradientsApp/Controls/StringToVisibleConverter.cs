d832d967ed518468f62ac384f02e0f5c04c83fcb
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2021-04-26 13:12:50 +0200

    Use Magic Gradients v1.3 (#1)
    * Add MagicGradients v1.3 with masks samples
    
    * Update samples
    
    * Add samples

---
+++ MagicGradientsApp/Controls/StringToVisibleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MagicGradientsApp.Controls
+{
+    public class StringToVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value?.ToString();
+            return !string.IsNullOrEmpty(text);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
